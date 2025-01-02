773228d64e8c08134dbc2b0c36b7709450867d28
seewruuk <sewruczekk@icloud.com>
2025-01-02 20:24:08 +0100

    stripe part 6, testing webhook

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,60 +3,59 @@ import { buffer } from 'micro';
 import Stripe from 'stripe';
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
 
 export async function POST(req, res) {
     const sig = req.headers['stripe-signature'];
     let event;
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    const body = await req.json();
+
 
     try {
         // Odczytanie bufora w sposób kompatybilny z Next.js
-        const buf = await buffer(req);
-        const rawBody = buf.toString(); // Konwertuj na string dla Stripe
-        event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        // const buf = await buffer(req);
+        // const rawBody = buf.toString(); // Konwertuj na string dla Stripe
+        // event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        return NextResponse.json({ status: 200, message: "Webhook received", body: body});
     } catch (err) {
-        console.error(`Error verifying webhook: ${err.message}`);
-        return NextResponse.json({ status: 'error', message: `Webhook Error: ${err.message}` });
+        // console.error(`Error verifying webhook: ${err.message}`);
+        // return NextResponse.json({ status: 'error', message: `Webhook Error: ${err.message}` });
+        return NextResponse.json({ status: 400, message: "Webhook not received", error: err.message});
+
     }
 
     // Logowanie poprawnie zbudowanego zdarzenia
-    console.log('Webhook event:', event);
+    // console.log('Webhook event:', event);
+    //
+    // if (event.type === 'checkout.session.completed') {
+    //     const session = event.data.object;
+    //
+    //     const sessionId = session.id;
+    //     const clientReferenceId = session.client_reference_id;
+    //
+    //     console.log(`Płatność zakończona: ${sessionId}`);
+    //     console.log(`Client Reference ID: ${clientReferenceId}`);
+    //
+    //     try {
+    //         const updateOrder = await fetch(`https://galeria-mentis.vercel.app/api/updateOrder`, {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({ orderId: clientReferenceId, orderStatus: "paid" }),
+    //         });
+    //
+    //         const sanityData = await updateOrder.json();
+    //         if (sanityData.status === "ok") {
+    //             return NextResponse.json({ status: 200, message: "Order status updated" });
+    //         } else {
+    //             return NextResponse.json({ status: 500, message: "Order status not updated" });
+    //         }
+    //     } catch (updateError) {
+    //         console.error("Error updating order:", updateError);
+    //         return NextResponse.json({ status: 500, message: "Order status not updated" });
+    //     }
+    // }
 
-    if (event.type === 'checkout.session.completed') {
-        const session = event.data.object;
-
-        const sessionId = session.id;
-        const clientReferenceId = session.client_reference_id;
-
-        console.log(`Płatność zakończona: ${sessionId}`);
-        console.log(`Client Reference ID: ${clientReferenceId}`);
-
-        try {
-            const updateOrder = await fetch(`https://galeria-mentis.vercel.app/api/updateOrder`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ orderId: clientReferenceId, orderStatus: "paid" }),
-            });
-
-            const sanityData = await updateOrder.json();
-            if (sanityData.status === "ok") {
-                return NextResponse.json({ status: 200, message: "Order status updated" });
-            } else {
-                return NextResponse.json({ status: 500, message: "Order status not updated" });
-            }
-        } catch (updateError) {
-            console.error("Error updating order:", updateError);
-            return NextResponse.json({ status: 500, message: "Order status not updated" });
-        }
-    }
-
-    return NextResponse.json({ status: 200, message: "Webhook received" });
 }
