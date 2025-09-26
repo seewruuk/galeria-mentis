5689b5bc4944f5e31967e2524a03574c8019987a
seewruuk <sewruczekk@icloud.com>
2025-09-26 16:23:37 +0200

    refactor: seo show p2

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,6 @@
-"use client"
 import Link from "next/link";
-import {useRouter} from "next/navigation";
 
 export default function Page() {
-
-    const router = useRouter();
-    // useEffect(() => {
-    //     router.push("/dashboard/orders");
-    // }, []);
-
 
     return (
         <div className={"h-screen w-screen grid place-items-center"}>
